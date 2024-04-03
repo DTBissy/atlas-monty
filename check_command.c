1a820882f1ea454ab0397711d57faddad4ed26cb
{DTBissy} <{isaactheedwards@gmail.com}>
2024-04-03 16:52:53 -0500

    Minor corrections

--- check_command.c
+++ check_command.c
@@ -12,11 +12,11 @@ void(*check_command(char *command))(stack_t **, unsigned int)
   };
   unsigned int i;
 
-  for (i = 0; i < sizeof(inst_t) / sizeof(instruction_t)); i++)
+  for (i = 0; i < sizeof(ins_t) / sizeof(instruction_t); i++)
   {
-    if (strcmp(command, ins_t[i]. name)== 0)
+    if (strcmp(command, ins_t[i].opcode)== 0)
     {
-      return (ins_t[i].f)
+      return (ins_t[i].f);
     }
   }
   return (NULL);
